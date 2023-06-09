1e2a00e671cc05072d7fa71acad61b73a2dd11e2
Rodrigo Rodriguez <gothenk@Rodrigos-iMac.local>
2023-06-09 11:58:18 -0500

    End of typescript

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,16 +1,20 @@
-import Card from "../card/card.component";
-import "./card-list.styles.css"
+import React from "react";
 
-const CardList = ({ monsters }) => (
-    <div className="card-list">
-        {
-            monsters.map((monster) => {
-                return (
-                    <Card key={monster.id} monster={monster} />
-                );
-            })
-        }
-    </div>
+import { Monster } from "../../App";
+
+import Card from "../card/card.component";
+import "./card-list.styles.css";
+
+type CardListProps = {
+  monsters: Monster[];
+};
+
+const CardList = ({ monsters }: CardListProps) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
 );
 
 // class CardList extends Component {
--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,18 +1,26 @@
+import React from "react";
+
+import { Monster } from "../../App";
+
 import "./card.styles.css";
 
-const Card = ({ monster }) => {
-    const {name, email, id} = monster;
-    return (
-        <div className="card-container">
-            <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-            />
-            <h2>{name}</h2>
-            <p>{email}</p>
-        </div>
-    );
-}
+type CardProps = {
+  monster: Monster;
+};
+
+const Card = ({ monster }: CardProps) => {
+  const { name, email, id } = monster;
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 // class Card extends Component {
 //     render() {
